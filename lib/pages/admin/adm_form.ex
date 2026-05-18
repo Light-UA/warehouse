d8d87e5fccf68b554493bf581934330117a0f8ec
Namdak Tonpa <maxim@synrc.com>
2026-05-19 00:35:56 +0300

    css

--- lib/pages/admin/adm_form.ex
+++ lib/pages/admin/adm_form.ex
@@ -2,15 +2,33 @@ defmodule ADM.FORM do
    require N2O
    require NITRO
 
-   def event({:client,{:form,mod}}) do
-       :nitro.insert_bottom(:stand, NITRO.h3(body: :nitro.to_binary(mod)))
-       :nitro.insert_bottom(:stand, NITRO.h5(body: mod.doc(), style: "margin-bottom: 10px;"))
-       :nitro.insert_bottom(:stand, NITRO.panel(:form.new(mod.new(mod,mod.id(),[]), mod.id()), class: :form))
+   def event({:client, {:form, mod, col_id}}) do
+       :nitro.insert_bottom(col_id, NITRO.panel(
+           class: "form-card",
+           body: [
+               NITRO.h3(body: :nitro.to_binary(mod)),
+               NITRO.h5(body: mod.doc(), style: "margin-bottom: 10px;"),
+               NITRO.panel(:form.new(mod.new(mod,mod.id(),[]), mod.id()), class: :form)
+           ]
+       ))
    end
 
    def event(:init) do
        :nitro.clear(:stand)
-       :lists.map(fn x -> send(self(),{:client,{:form,x}}) end, :application.get_env(:form, :registry, []))
+       :nitro.insert_bottom(:stand, NITRO.panel(id: :col1, class: "form-column"))
+       :nitro.insert_bottom(:stand, NITRO.panel(id: :col2, class: "form-column"))
+       :nitro.insert_bottom(:stand, NITRO.panel(id: :col3, class: "form-column"))
+
+       :application.get_env(:form, :registry, [])
+       |> Enum.with_index()
+       |> Enum.each(fn {mod, index} ->
+           col_id = case rem(index, 3) do
+               0 -> :col1
+               1 -> :col2
+               2 -> :col3
+           end
+           send(self(), {:client, {:form, mod, col_id}})
+       end)
    end
 
    def event(_), do: :ok
